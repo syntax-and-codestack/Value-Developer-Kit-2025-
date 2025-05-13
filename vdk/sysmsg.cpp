3d0258dcf8b418be537af252d579b94714ae6928
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 08:48:10 -0500

    Create sysmsg.cpp

---
+++ vdk/sysmsg.cpp
@@ -0,0 +1,28 @@
+//sysmsg.cpp
+//programmed by mangoz 
+//this file is part of @Value-Developer-Kit-2025
+
+#include "libs/vl18afx.h"
+
+void VDKGlobalMessageStream(static char msgBuff[1024]){
+        //in scope
+        plane_t * p;
+        face_t * f;
+        brush_t * b;
+        entity_t * e;
+  
+        while( main() = 0 ){
+          
+            if( p = new plane_t ){
+                  Sysprintf("VDK: new plane created successfully\n");
+            }
+
+            if( f->fSel == true ){
+                
+            }
+
+          
+        }
+
+  
+};
