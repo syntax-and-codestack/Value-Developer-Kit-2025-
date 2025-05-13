faa11f53c115e7d9b3545040cdb59d55ee6e322a
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 07:57:01 -0500

    Create bckgrnd2d.h

---
+++ plugins/bckgrndv2d/bckgrnd2d.h
@@ -0,0 +1,32 @@
+//bckgrnd2d.h
+//programmed by mangoz 
+//this file is part of @Value-Developer-Kit-2025
+
+#include "libs/cmdlib.h"
+#include "libs/mathlib.h"
+
+#ifndef BCKGRND2D_H
+#define BCKGRND2D_H
+
+vec3_t gridView_vectors[16];
+
+class BckGrnd2DTool{
+public:
+   BckGrnd2DTool();
+   ~BckGrnd2DTool();
+
+   int m2DCols;
+   int m2DRows;
+
+   bool ZoomIn();
+   bool ZoomOut();
+
+   void * m2DGridRelease(){
+      delete []m2DCols;
+      delete []mwDRows;
+   };
+};
+
+BckGrnd2DTool * 2DView();
+
+#endif
