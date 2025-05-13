f95b2bb761b852f251ea1bc36d20d2ba6add3bcb
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 10:25:54 -0500

    Update camera.h

--- vdk/camera.h
+++ vdk/camera.h
@@ -10,6 +10,23 @@ public:
  camera_t();
  ~camera_t();
 
+   vec3_t origin;
+   vec3_t angles;
+   vec3_t view;
+
+   vec_t m_projectPoint[3];
+
+   bool bCameraFakeLighting;
+   bool bCameraRealLighting;
+
+   void ShowEntity( entity_t * e );
+   void ShowBrushes( brush_t * b );
+   void ShowTextures( texdef_t * t );
+   void Camera_ProjectMatrix( vec3_t m4x4[16] );
+   void ShowFaces( face_t * f );
+
+   void UpdateCamera( camera_t * camera );
+
 };
 
 #endif
