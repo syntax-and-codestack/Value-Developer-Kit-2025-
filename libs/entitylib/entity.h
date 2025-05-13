c9c3562009bfb7f0db22730c9fa096cd9f458b75
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 08:14:17 -0500

    Update entity.h

--- libs/entitylib/entity.h
+++ libs/entitylib/entity.h
@@ -1,2 +1,23 @@
 //entity.h
-//programmed by mangoz
+//programmed by mangoz 
+//this file is part of @Value-Developer-Kit-2025
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "libs/mathlib.h"
+
+class IEntity;
+class IEntityStatic;
+class IEntityTransformable;
+
+typedef struct entity_s{
+    const char* eKey;
+    const char* eValue;
+    vec3_t position;
+    bool eDrawAABB;
+    int enumberId;
+    entity_t * prev, *owner, *next;
+}entity_t;
+
+#endif
