501f7a3c5fe57129a8cd6c92137d22621bd8abe3
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 10:01:57 -0500

    Update vl18afx.h

--- libs/vl18afx.h
+++ libs/vl18afx.h
@@ -8,3 +8,30 @@
 #include <string>
 #include <Windows.h>
 #include <GL.h>
+
+class CWndGuiTable{
+public:
+  CWndGuiTable();
+
+  HWND * d_MainWnd;//main window
+  HWND * d_CameraWnd;//camera window
+  HWND * d_GridWnd;//grid window holders
+  HWND * d_XYWnd;//xy grid window
+  HWND * d_XZWnd;//xz grid window
+  HWND * d_YZWnd;//yz grid window
+  HWND * d_TextureWnd;//texture window
+
+  //dialogs...
+  HWND * d_FilterDlg;
+  HWND * d_BPDlg;
+  HWND * d_CapDlg;
+  HWND * d_NodeDlg;
+
+  //toolbar
+  HWND * d_Toolbar;
+
+  //menu
+  HMENU * d_Menu;
+};
+
+CWndGuiTable * AddWidget();
