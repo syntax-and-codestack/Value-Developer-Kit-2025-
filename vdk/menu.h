d16d37e58b2e2e1be1ad2772b325e7d32f0805b2
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 10:08:34 -0500

    Create menu.h

---
+++ vdk/menu.h
@@ -0,0 +1,21 @@
+//menu.h
+
+#include "libs/vl18afx.h"
+
+#ifndef MENU_H
+#define MENU_H
+
+//probally exceed anyways...
+#define MAX_MENU_ITEMS 12 
+
+const char* FILE_MENU = "&File";
+const char* FILE_EDIT = "&Edit";
+const char* FILE_BSP = "&Bsp";
+const char* FILE_TEXTURE = "&Texture";
+const char* FILE_BRUSH = "&Brush";
+const char* FILE_MISC = "&Misc";
+const char* FILE_FILTER = "&Filter";
+const char* FILE_PLUGIN = "&Plugin";
+const char& FILE_HELP = "&Help";
+
+#endif
