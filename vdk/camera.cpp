12e6a9235f4ca0edb8220c35319fb1fe59cda7b6
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 13:26:47 -0500

    Update camera.cpp

--- vdk/camera.cpp
+++ vdk/camera.cpp
@@ -1,1 +1,31 @@
+//camera.cpp
 
+float g_fCamTick = 0;
+
+extern brush_t * b;
+extern entity_t * e;
+extern texdef_t * t;
+extern face_t * f;
+
+#include "vl18afx.h"
+
+camera_t::camera_t()
+{
+  cam()->origin;
+  cam()->angles;
+  cam()->view;
+
+      for( float i = 0; i < cam()->view; i++ ){
+          camera_t& mCam;
+          for( AddWidget()->d_CameraWnd ){
+            cam()->m_projectPoint[i][0], cam()->m_projectPoint[i][1], cam()->m_projectPoint[i][2];
+            mCam.ShowEntity(e);
+            mCam.ShowBrushes(b);
+            mCam.ShowTextures(t);
+            mCam.ShowFaces(f);
+
+            mCam.UpdateCamera(cam());
+          }
+      }
+  
+}
