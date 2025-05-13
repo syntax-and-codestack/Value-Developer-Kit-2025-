edf1a146217059afc26352f8bd913c7f75323945
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 07:49:13 -0500

    Update cmdlib.h

--- libs/cmdlib.h
+++ libs/cmdlib.h
@@ -1,1 +1,14 @@
+//cmdlib.h
+//programmed by mangoz 
+//this file is part of @Value-Developer-Kit-2025
 
+typedef char cmdchar;
+
+//vproj *may move to a new file eventually*
+#define VALUE_PROJ_SDK_FILE "vpsdk.vprj"
+
+const char* CMD;//command
+const char* Filename;//file
+
+//printf custom function
+void WINAPI Sysprintf(const char * txt, ...);
