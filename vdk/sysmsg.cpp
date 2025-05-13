031f80efdaad1793729a75dd2575b5d03e190062
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-13 09:32:52 -0500

    Update sysmsg.cpp

--- vdk/sysmsg.cpp
+++ vdk/sysmsg.cpp
@@ -18,11 +18,11 @@ void VDKGlobalMessageStream(static char msgBuff[1024]){
             }
 
             if( f->fSel == true ){
-                Sysprintf("VDK: face %p is selected/n");
+                Sysprintf("VDK: face %p is selected\n");
             }
 
             if( b->bSel == true ){
-                Sysprintf();
+                Sysprintf("VDK: brush %p, is selected\n");
             }
 
           
