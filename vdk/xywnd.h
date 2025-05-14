cf3d19371b9b2411c002253cfe12ae04a7426499
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-14 09:18:27 -0500

    Update xywnd.h

--- vdk/xywnd.h
+++ vdk/xywnd.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#ifndef XYWND_H
+#define XYWND_H
+
+#include "vl18afx.h"
+
+class XYWnd{
+public:
+  XYWnd(HWND pWnd);
+  ~XYWnd(HWND pWnd);
+
+int width, height;
+int x, y;
+
+bool bXYClicked;
+virtual void XYPaintWnd() = 0;
+//will be an array 
+int nXYMouseClicks_Store;
+
+};
+
+#endif
