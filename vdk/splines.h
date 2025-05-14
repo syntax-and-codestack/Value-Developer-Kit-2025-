58e0836a05b617adfb94da7fb11e615cd586c282
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-14 07:39:43 -0500

    Create splines.h

---
+++ vdk/splines.h
@@ -0,0 +1,13 @@
+//splines.h
+
+#include "mathlib.h"
+
+#ifndef SPLINES_H
+#define SPLINES_H
+
+static vec4_t blue( 0, 0, 1, 1);
+static vec4_t red( 1, 0, 0, 1);
+
+extern void aaBoxDraw( vec4_t& color, float size );
+
+#endif
