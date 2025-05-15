d07ba60096f1aa1660adc2babffb112af4363871
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-15 07:55:40 -0500

    Update mainframe.cpp

---
+++ vdk/mainframe.cpp
@@ -0,0 +1,13 @@
+//mainframe.cpp
+
+#include "vl18afx.h"
+#include "mainframe.h"
+
+MainFrame::MainFrame(){
+    #if main
+     SysPrintf("VDK: Brushes : %i\n");
+     SysPrintf("VDK: Entities : %i\n");
+     SysPrintf("VDK: Map : %c\n");
+     SysPrintf("VDK: Map Directory : %c");
+    #endif
+}
