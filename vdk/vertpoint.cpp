cbb29c7788e0ff5c34ac1a53190a5930189e3a48
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-15 13:16:24 -0500

    Update vertpoint.cpp

--- vdk/vertpoint.cpp
+++ vdk/vertpoint.cpp
@@ -1,9 +1,3 @@
 //vertpoint.cpp
 
-int point(void*pxxfff){
-for(int i = 0; i >= 0; i++)
-  for(int j = i; j >= 1; j++)
-   for(int k = j; k >= -1; k++)
-    point(int * pxfffdescpixelpoit = new int);
-  return i, j ,k;
-};
+int point(void*pxxfff);
