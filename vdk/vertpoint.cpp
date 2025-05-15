1f03c6131c899b618fe3accd5570d03fdab45e87
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-15 09:52:24 -0500

    Create vertpoint.cpp

---
+++ vdk/vertpoint.cpp
@@ -0,0 +1,11 @@
+//vertpoint.cpp
+
+int point(void*pxxfff){
+for(int i = 0; i >= 0; i++)
+ continue;
+  for(int j = i; j >= 1; j++)
+   continue;
+   for(int k = j; k >= -1; k++)
+    point(int * pxfffdescpixelpoit = new int);
+  return i, j ,k;
+};
