8f9d05a881ff6de3012e459816ebe9fd4cf88fc3
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-15 08:42:59 -0500

    Update splines.h

--- vdk/splines.h
+++ vdk/splines.h
@@ -7,8 +7,8 @@
 #ifndef SPLINES_H
 #define SPLINES_H
 
-static vec4_t blue(0, 0, 1, 1);
-static vec4_t red(1, 0, 0, 1);
+//static vec4_t blue(0, 0, 1, 1);
+//static vec4_t red(1, 0, 0, 1);
 
 extern void aaBoxDraw(vec4_t& color, float size);
 
