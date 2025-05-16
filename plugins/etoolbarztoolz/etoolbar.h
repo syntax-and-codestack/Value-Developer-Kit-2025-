467540d0dcd341f5e74a261ea25a6469af748ce0
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-16 08:13:56 -0500

    Create etoolbar.h

---
+++ plugins/etoolbarztoolz/etoolbar.h
@@ -0,0 +1,28 @@
+//etoolbar.h
+
+#ifndef ETOOLBAR_H
+#define ETOOLBAR_H
+
+class EToolbar{
+public:
+    EToolbar();
+    ~EToolbar();
+
+    virtual int x() = 0;
+    virtual int y() = 0;
+    virtual int width() = 0;
+    virtual int height() = 0;
+
+    virtual unsigned char * EButtonImage() = 0;
+    virtual void CreateEToolbar(EToolbar * Toolbar, int buttons, int nStyle, int x, int y, int width, int height, const char* Name) = 0;
+    
+   enum
+   {
+      eButton,
+      eBmpButton,
+      eToggle,
+      eCheck
+   }EToolbarType;
+};
+
+#endif
