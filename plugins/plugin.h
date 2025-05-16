befbe3e27a221748faa5f2b166e225771cf96504
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-16 08:04:50 -0500

    Update plugin.h

--- plugins/plugin.h
+++ plugins/plugin.h
@@ -1,1 +1,17 @@
+//plugin.h
 
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+class CPlugin{
+public:
+   CPlugin();
+   ~CPlugin();
+
+    const char* CPLUGIN_NAME = "CPlugin VDK: %t";
+
+    virtual void CPluginSynapseClient(const char* CPluginName) = 0;
+
+};
+
+#endif
