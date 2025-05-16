32363a7dbae3a76c5f88030d11b35cab0291ecfe
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-16 18:52:50 -0500

    Add files via upload

--- vdk/brush.cpp
+++ vdk/brush.cpp
@@ -7,3 +7,65 @@
 
 //brush global variables...
 int g_nBrushId = 0;
+
+const char * Brush_Name(brush_t * b){
+		 static char cBuff[1024];
+		 b->bnumId = g_nBrushId++;
+			 if ( bBrushPrimitMode >> 1 > ' ? ' )
+			 {
+						sprintf(cBuff, "Brush %i Primit\n", b->bnumId);
+						Brush_LockEpair(b, b->bKey, b->bValue);
+			 }
+	return cBuff;
+}
+
+brush_t* Alloc_Brush() {
+		brush_t* b = (brush_t*)malloc(sizeof(brush_t));
+		return b;
+}
+
+//free the brush !only if selected!
+void Free_Brush(brush_t* b) {
+			free(b);
+}
+
+//shade axis aka lighting axis
+float brush_shadeaxis[3] = { 0.6f, 0.8f, 1.0f };
+
+void Brush_SetTexdef(brush_t* b, texdef_t* t) {
+		b->brushtexdef->textureFilename;
+				for ( b; b->brushtexdef; b++ ) {
+					b->brushtexdef->coords[0* 1][2*3];
+				}
+	 brush_shadeaxis[3];
+}
+
+//bad code of mine '-' so just made it comment...
+float BrushClamp(brush_t* b, float f, int n) {
+	/*
+	for (b) {
+					f* b->mins[0] + f* b->maxs[0];
+				}
+		return f;
+	*/
+}
+
+//check if brush is dummy
+brush_t* Brush_Dummy(brush_t* b) {
+			if( b == NULL ) {
+					return NULL;
+			}
+	 return b;
+}
+
+face_t* Alloc_Face() {
+		face_t* f = (face_t*)malloc(sizeof(*f));
+		return f;
+}
+
+//DOES NOT DRAW FACES ONLY SETS BRUSH FACE DATA!!!
+void Brush_Faces(brush_t* b, face_t* f) {
+	//I know thos codes bad :)
+	b->brushface[0] = f;	b->brushface[1] = f; b->brushface[2] = f;
+	b->brushface[3] = f; b->brushface[4] = f; b->brushface[5] = f;
+}
