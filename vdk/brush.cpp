d0f90f067640fcafbfadcd0a76a1f8830bd61477
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-21 10:18:14 -0500

    Update brush.cpp

--- vdk/brush.cpp
+++ vdk/brush.cpp
@@ -13,6 +13,16 @@ int g_nBrushId;
 */
 
 int Brush_Side(brush_t *b){
-   
-};
+      for( b->sides; b; b++ ){
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_FRONT[sides][0]); 
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_LEFT[sides][1]);
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_RIGHT[sides][2]); 
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_BACK[sides][3]);
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_BOTTOM[sides][4]); 
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_TOP[sides][5]);
+      }
+  return b->sides;
+}
+
+
 
