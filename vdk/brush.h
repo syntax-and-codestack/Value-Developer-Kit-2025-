ff8e6120647c9eb9bcac3254b26dbdf7f6401a13
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-22 14:11:15 -0500

    Update brush.h

--- vdk/brush.h
+++ vdk/brush.h
@@ -4,22 +4,22 @@
 
 #include "vl18afx.h"
 
-brush_t* Alloc_Brush();
-brush_t* Brush_SetCount(brush_t* b);
-brush_t* Brush_Dummy(brush_t* b);
-std::size_t BrushSize();
+brush_t* Alloc_Brush();//done
+brush_t* Brush_SetCount(brush_t* b);//done
+brush_t* Brush_Dummy(brush_t* b);//done
+std::size_t BrushSize();//done
 plane_t* Plane_SetCount(brush_t* b, plane_t * p);
 face_t* Face_SetCount(brush_t* b, face_t * f);
 const char* BrushFace_Name(face_t * f);
 const char* BrushPlane_Name(plane_t * p);
-const char* Brush_Name(brush_t* b);
-char* Brush_Buffer(brush_t* b);
+const char* Brush_Name(brush_t* b);//done
+char* Brush_Buffer(brush_t* b);//done
 face_t* Face_Dummy(brush_t* b, face_t * f);
 plane_t* Plane_Dummy(brush_t* b, plane_t* p);
 face_t* Alloc_Face();
 plane_t* Alloc_Plane();
 void Construct_Brush(brush_t* b, vec3_t bMins, vec3_t bMaxs, bool bSel, texdef_t* texture);
-brush_t* Create_Brush(brush_t* b);
+brush_t* Create_Brush(brush_t* b);//done
 bool BrushIsSelected(brush_t* b);
 bool Brush_FaceSelected(face_t* f, brush_t* b);
 bool bBrushPrimitMode;//single use not member
@@ -34,8 +34,8 @@ void Brush_MakeFace(brush_t* b, face_t* f);
 void Drag_BrushSel(brush_t* b);
 void Brush_AddToBspList(brush_t* b, brush_t* blst);
 void Brush_BspBuild(brush_t* b);
-brush_t* Brush_Copy(brush_t* b);
-brush_t* Brush_FullCopy(brush_t* b);
+brush_t* Brush_Copy(brush_t* b);//done
+brush_t* Brush_FullCopy(brush_t* b);//done
 void Brush_Resize(brush_t* b, vec3_t bMin, vec3_t bMax);
 void Brush_DrawFace(brush_t* b, face_t* f, int GL);
 void Brush_DrawXY(brush_t* b, int view_t);
@@ -48,7 +48,7 @@ void Brush_MoveSel(brush_t* b, const vec3_t movesel);
 bool Brush_bCaulked(brush_t* b, texdef_t* t);
 void Brush_Rotate(brush_t* b, vec3_t bAngle, vec3_t bOrigin);
 void Brush_DeleteDummyFaces(brush_t* b);
-void Brush_LockEpair(brush_t* b, const char* key, const char* value);
+void Brush_LockEpair(brush_t* b, const char* key, const char* value);//done
 int MakePlanePoint(float* f);
 void Plane_Free(plane_t* p);
 void Face_Free(face_t* f);
@@ -57,7 +57,7 @@ face_t* Face_FullCopy(face_t* f);
 void Face_SetTexture(face_t* f, const char pName);
 //get the brush name and string to list... maybe function might change
 const std::string Brush_String( brush_t * b );
-int Brush_Side( brush_t * b );
+int Brush_Side( brush_t * b );//done
 vec_t Brush_SarrusDet(vec3_t a, vec3_t b, vec3_t c);
 void BP_BrushSet_MatTexdefCoords( brush_t * b, vec3_t MA[3], vec3_t MB[3] );
 void Brush_Print( brush_t * b );
@@ -72,7 +72,6 @@ brush_t * Brush_Tall(brush_t * b);
 void Compute_BrushBaseAxis( brush_t * b );
 brush_t * Brush_MakePatch( brush_t * b );
 int Brush_SetSpawnflags( brush_t * b );
-static char * Brush_Buffer(brush_t * b);
 brush_t * pConvert_Mesh(brush_t * b);
 void Brush_Bounds(brush_t * b, vec3_t MB[4], vec3_t MC[4]);
 brush_t * Brush_CovertCSG(brush_t * b);
