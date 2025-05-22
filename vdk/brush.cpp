b9cd6881ae8f1569d3c33914af081ac2828b8ff2
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-22 08:16:26 -0500

    Update brush.cpp

--- vdk/brush.cpp
+++ vdk/brush.cpp
@@ -11,15 +11,11 @@ int g_nBrushId = 0;
 /*
  Brush Sides
 */
-
 int Brush_Side(brush_t *b){
       for( b->sides; b; b++ ){
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_FRONT[sides][0]); 
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_LEFT[sides][1]);
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_RIGHT[sides][2]); 
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_BACK[sides][3]);
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_BOTTOM[sides][4]); 
-         b->brush_sidewindings()->Side_WindingsFace(SIDE_TOP[sides][5]);
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_FRONT[b->sides][0]); b->brush_sidewindings()->Side_WindingsFace(SIDE_LEFT[b->sides][1]);
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_RIGHT[b->sides][2]); b->brush_sidewindings()->Side_WindingsFace(SIDE_BACK[b->sides][3]);
+         b->brush_sidewindings()->Side_WindingsFace(SIDE_BOTTOM[b->sides][4]); b->brush_sidewindings()->Side_WindingsFace(SIDE_TOP[b->sides][5]);
       }
   return b->sides;
 }
