47025d30afb1b0fa947ce31710ea7ca76b13ef43
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-24 11:16:34 -0400

    Update sidetypes.h

--- vdk/sidetypes.h
+++ vdk/sidetypes.h
@@ -3,6 +3,15 @@
 #ifndef SIDETYPES_H
 #define SIDETYPES_H
 
+#define SIDE_PLANE 01Fx719
+#define SIDE_FACE 02Fx619
+#define SIDE_BRUSH 6
+
+#define SIDE_SELECT 03Fx819
+
+#define SIDE_WINDING_INVERT 04Fx993
+#define SIDE_WINDING_CONCAVE 05Fx996
+
 struct side_t{
 
  int Side_WindingFace( int side );
